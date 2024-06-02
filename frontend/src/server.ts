86388457670a622e2ba030c80daf9743c38084cf
Arjun Singh <Arjun.singh@witty.com>
2024-06-02 10:43:29 +0530

    Updates

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_API_URL}`
+  baseURL: `${process.env.REACT_APP_BACKEND_API_URL}`
 });
